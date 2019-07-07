9d174daacbd519616d7f29e666627aa2b1de0226
y276lin <y276lin@edu.uwaterloo.ca>
2019-07-07 18:32:25 -0400

    Extract Frames from videos

--- controllers/recommend_controller.js
+++ controllers/recommend_controller.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs')
 const Catalog = JSON.parse(fs.readFileSync('./songs/songs.json', 'utf8'));
+const extractFrames = require('ffmpeg-extract-frames')
+const { getVideoDurationInSeconds } = require('get-video-duration')
 
 // export interface Playlist {
 //     uri: string,
@@ -14,9 +16,27 @@ async function validate_media_path(path) {
     return await fs.existsSync('./uploads/' + path)
 }
 
+async function extract_frames(file_name) {
+    const duration = await getVideoDurationInSeconds('./uploads/C4604DF7-1665-438E-A78C-FDB196BBD9A7.mov')
+    console.log('duration', duration)
+
+    // remove .jpg and .mov
+    const file_name_no_ext = file_name.slice(0, file_name.length - 4)
+
+    await extractFrames({
+        input: './uploads/' + file_name,
+        output: `./uploads/${file_name_no_ext}-${i}.jpg`,
+        offsets: [
+            1000,
+            2000,
+        ]
+    })
+}
+
 router.get('/:file_path', async (req, res) => {
     const file_exists = await (validate_media_path(req.params.file_path))
     console.log('[Recommendation][file_path][exists]', req.params.file_path, file_exists)
+
 
     const playlist = []
     const tabu = new Set()
