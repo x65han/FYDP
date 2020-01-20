d3bfe794947b14f2b9fab492c6a47d23409e6d79
x65han <x65han@edu.uwaterloo.ca>
2020-01-19 19:49:57 -0500

    scripts/install-aws.sh

--- bundle.sh
+++ bundle.sh
@@ -7,5 +7,12 @@ mkdir -p ../mudio2/controllers
 cp controllers/*.py ../mudio2/controllers/
 cp -r ml_prod ../mudio2/
 cp -r songs ../mudio2/
+cp -r scripts ../mudio2/
 cp -r uploads ../mudio2/
 cp -r templates ../mudio2/
+
+# upload
+cd ~/
+zip -r mudio2.zip mudio2
+callisto up ~/mudio2.zip
+callisto url mudio2.zip
---
+++ scripts/install-aws.sh
@@ -0,0 +1,4 @@
+# https://www.youtube.com/watch?v=oOqqwYI60FI
+source activate tensorflow_p36
+conda install -y imageio
+pip install moviepy
